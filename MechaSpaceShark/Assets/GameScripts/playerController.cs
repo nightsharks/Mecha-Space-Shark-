50295960d2fa8eba2dcfd8b17cc7635d07c67ec2
nightsharks <mandoyastar@gmail.com>
2018-11-03 14:34:37 +0000

    updating unity to 2.14

--- MechaSpaceShark/Assets/GameScripts/playerController.cs
+++ MechaSpaceShark/Assets/GameScripts/playerController.cs
@@ -16,22 +16,23 @@ public class playerController : MonoBehaviour {
         transform.Translate(y, x, 0);
     }
 
-    void OnTriggerEnter(Collider other)
+    public void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Consumable"))
+        if (other.gameObject.CompareTag ("Obstacle"))
         {
             other.gameObject.SetActive(false);
+            Debug.Log("this collided");
             scoreCount = scoreCount + 1;
             scoreText();
         }
 
-        else if (other.gameObject.CompareTag("Obstacle"))
-        {
-            Application.Quit();
-        }
+       // else if (other.gameObject.CompareTag("Obstacle"))
+     //   {
+      //      Debug.Log("this collided with rock");
+       // }
     }
 
-    void scoreText()
+    public void scoreText()
     {
         displayScore.text = "Score: " + scoreCount.ToString();
     }
