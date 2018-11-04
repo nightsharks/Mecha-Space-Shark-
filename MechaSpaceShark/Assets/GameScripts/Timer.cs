d2ce7f1546ef68e2de8f4e005fb5c086ae00edfe
nightsharks <mandoyastar@gmail.com>
2018-11-04 19:08:21 +0000

    adding the timer

---
+++ MechaSpaceShark/Assets/GameScripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+
+public class Timer : MonoBehaviour {
+
+    public float timer = 0;
+    public Text counter;
+
+  void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        counter.text = "Time: " + timer.ToString("F2");
+    }
+
+}
