426fb968525edf1797a966054d7083ec1679d900
nightsharks <mandoyastar@gmail.com>
2018-11-12 20:02:51 +0000

    started the invincibility spawn script

---
+++ MechaSpaceShark/Assets/GameScripts/InvincibilitySpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvincibilitySpawner : MonoBehaviour {
+
+    public GameObject invincibilityItem;
+    public GameObject player;
+
+    private float spawnMin = 1f; 
+    private float spawnMax = 50f; 
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    private void Update()
+    {
+        if (player.transform.position.x - 300 > this.transform.position.x)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    void Spawn()
+    {
+        Instantiate(invincibilityItem, transform.position, Quaternion.identity);
+        Invoke("Spawn", Random.Range(spawnMin, spawnMax));
+    }
+}
